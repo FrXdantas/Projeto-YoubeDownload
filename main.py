ff80161efe21b509ec9a3e70d58e81eac1df5de0
unknown <frankrdantas@gmail.com>
2023-01-27 19:46:47 -0400

    Codigo modificado

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-#arquivo inicial para o programa de download de videos do yotube.com
-#programador: franklin dantas
-#Manaus, Brasil
+#biblioteca que trabalha com youtube
+from pytube import YouTube 
 
+def Download (link):
+    youtubeObjeto = YouTube(link)
+    youtubeObjeto = youtubeObjeto.streams.get_highest_resolution()
+    youtubeObjeto = youtubeObjeto.
+    
+    try:
+        youtubeObjeto.download()
+    except:
+        print('Erro ao baixar video')
+    print('Download do Video do Youtube Completo')
+        
+   
